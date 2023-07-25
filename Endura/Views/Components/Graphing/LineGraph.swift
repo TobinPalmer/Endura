84c83b3c1baaa863c1ab10135dea08896bc2e1dc
TobinPalmer <tobinkpalmer@gmail.com>
2023-07-24 22:41:25 -0700

    Clean graph dragging animation

--- Endura/Views/Components/Graphing/LineGraph.swift
+++ Endura/Views/Components/Graphing/LineGraph.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 public struct LineGraph: View {
+    @State private var touchLocation: CGFloat? = nil
+
     private let data: [Double]
 
     public init(data: [Double]) {
@@ -14,31 +16,58 @@ public struct LineGraph: View {
     }
 
     public var body: some View {
-        let max = data.max() ?? 0
-        let min = data.min() ?? 0
-        let range = max - min
+        let maxVal = data.max() ?? 0
+        let minVal = data.min() ?? 0
+        let range = maxVal - minVal
 
-        GeometryReader { geometry in
+        VStack {
+            GeometryReader { geometry in
+                let frame = geometry.frame(in: .local)
+                let stepWidth = frame.width / CGFloat(data.count - 1)
+                let stepHeight = frame.height / CGFloat(range)
 
-            let frame = geometry.frame(in: .local)
-            let stepWidth = frame.width / CGFloat(data.count - 1)
-            let stepHeight = frame.height / CGFloat(range)
+                Path { path in
+                    for index in data.indices {
+                        let xPosition = stepWidth * CGFloat(index)
+                        let yPosition = stepHeight * CGFloat((data[index] - minVal))
 
-            Path { path in
-                for index in data.indices {
-                    let xPosition = stepWidth * CGFloat(index)
-                    let yPosition = stepHeight * CGFloat((data[index] - min))
+                        let point = CGPoint(x: xPosition, y: frame.height - yPosition)
 
-                    let point = CGPoint(x: xPosition, y: frame.height - yPosition)
-
-                    if index == 0 {
-                        path.move(to: point)
-                    } else {
-                        path.addLine(to: point)
+                        if index == 0 {
+                            path.move(to: point)
+                        } else {
+                            path.addLine(to: point)
+                        }
                     }
                 }
+                    .stroke(Color.primary, lineWidth: 2)
+
+                if let touchLocation = touchLocation {
+                    let index = min(max(Int((touchLocation / stepWidth).rounded()), 0), data.count - 1)
+                    let value = data[index]
+                    let yPosition = stepHeight * CGFloat((value - minVal))
+
+                    Circle()
+                        .fill(Color.primary)
+                        .frame(width: 10, height: 10)
+                        .position(CGPoint(x: touchLocation, y: frame.height - yPosition))
+
+                    Text("\(data[index])")
+                        .position(CGPoint(x: touchLocation, y: frame.height - yPosition - 30))
+                }
             }
-                .stroke(Color.primary, lineWidth: 2)
         }
+            .background(Color.clear)
+            .frame(height: 200)
+            .border(Color.red)
+            .contentShape(Rectangle())
+            .gesture(DragGesture(minimumDistance: 0)
+                .onChanged({ value in
+                    self.touchLocation = value.location.x
+                })
+                .onEnded({ _ in
+                    self.touchLocation = nil
+                })
+            )
     }
 }
