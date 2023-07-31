7d3774e5435e449fee4557616c5d41c088f6bd5f
TobinPalmer <tobinkpalmer@gmail.com>
2023-07-31 09:06:03 -0700

    Infinite scroll view started

--- Endura/Views/ProgressView.swift
+++ Endura/Views/ProgressView.swift
@@ -5,9 +5,47 @@
 import Foundation
 import SwiftUI
 
-public struct ProgressDashboardView: View {
+import SwiftUI
 
-    public var body: some View {
-        Text("Progress View")
+struct ProgressDashboardView: View {
+    @ObservedObject var viewModel = ProgressDashboardViewModel()
+
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(viewModel.items.indices, id: \.self) { index in
+                    Text("Item Number \(viewModel.items[index])")
+                        .onAppear {
+                            if index == viewModel.items.count - 5 {
+                                viewModel.loadMoreContent()
+                            }
+                        }
+                        .frame(height: 250)
+                }
+            }
+        }
+            .onAppear {
+                viewModel.setup()
+            }
+    }
+}
+
+class ProgressDashboardViewModel: ObservableObject {
+    @Published var items: [Int] = []
+    var currentCount = 0
+    let incrementAmount = 5
+    let maxIncrementCount = 10
+    var currentIncrementCount = 0
+
+    func setup() {
+        items = Array(0..<30)
+        currentCount = items.count
+    }
+
+    func loadMoreContent() {
+        print("Loading more context for \(currentCount)")
+        let range = currentCount..<(currentCount + incrementAmount)
+        items.append(contentsOf: range)
+        currentCount += incrementAmount
     }
 }
