b9eb58305a50a5d3e8ff4a8868938603f596e4c9
TobinPalmer <tobinkpalmer@gmail.com>
2023-08-02 11:15:53 -0700

    Updated calendar to have new switching popup

--- Endura/Views/Components/Schedule/ScheduleDay.swift
+++ Endura/Views/Components/Schedule/ScheduleDay.swift
@@ -5,12 +5,49 @@
 import Foundation
 import SwiftUI
 
-struct SheetView: View {
+//struct CalanderPopupSheetView: View {
+//    @Environment(\.dismiss) var dismiss
+//    private var date: Date
+//
+//    init(day: Date) {
+//        date = day
+//    }
+//
+//    var body: some View {
+//        Button("Press to dismiss") {
+//            dismiss()
+//        }
+//        let todayDate = Calendar.current.dateComponents([.day], from: date)
+//        let startOfWeek = CalendarUtils.startOfWeek(for: date)
+//        let datesOfWeek = (0..<7).compactMap {
+//            Calendar.current.date(byAdding: .day, value: $0, to: startOfWeek)
+//        }
+//        HStack {
+//            ForEach(datesOfWeek, id: \.self) { date in
+//                let day = Calendar.current.component(.day, from: date)
+//                if day == todayDate.day! {
+//                    Text("\(day)")
+//                        .font(.title3)
+//                        .padding()
+//                        .background(Color.yellow)
+//                } else {
+//                    Text("\(day)")
+//                        .font(.title3)
+//                        .padding()
+//                }
+//            }
+//        }
+//        Text("Current Date: \(date)")
+//        Spacer()
+//    }
+//}
+
+struct CalanderPopupSheetView: View {
     @Environment(\.dismiss) var dismiss
-    private var date: Date
+    @State private var date: Date
 
     init(day: Date) {
-        date = day
+        _date = State(initialValue: day)
     }
 
     var body: some View {
@@ -22,23 +59,39 @@ struct SheetView: View {
         let datesOfWeek = (0..<7).compactMap {
             Calendar.current.date(byAdding: .day, value: $0, to: startOfWeek)
         }
-        VStack {
+        HStack {
             ForEach(datesOfWeek, id: \.self) { date in
                 let day = Calendar.current.component(.day, from: date)
-                if day == todayDate.day! {
-                    Text("\(day)")
-                        .font(.title)
-                        .padding()
-                        .background(Color.yellow)
-                } else {
-                    Text("\(day)")
-                        .font(.title)
-                        .padding()
+                Button {
+                    self.date = date
                 }
+                label: {
+                    if day == todayDate.day! {
+                        Text("\(day)")
+                            .padding()
+                            .background(Color.yellow)
+                    } else {
+                        Text("\(day)")
+                            .padding()
+                            .background(isSameMonth(date1: date, date2: Date()) ? Color.white : Color.gray)
+                    }
+                }
+                    .disabled(!isSameMonth(date1: date, date2: Date()))
+                    .frame(minWidth: 0, maxWidth: .infinity)
             }
         }
+        Text("Current Date: \(date)")
+        Spacer()
+    }
+
+    func isSameMonth(date1: Date, date2: Date) -> Bool {
+        let calendar = Calendar.current
+        let month1 = calendar.component(.month, from: date1)
+        let month2 = calendar.component(.month, from: date2)
+        return month1 == month2
     }
 }
+
 
 struct DayView: View {
     private let date: Date
@@ -69,7 +122,7 @@ struct DayView: View {
                 }
             }
             .sheet(isPresented: $showingSheet) {
-                SheetView(day: date)
+                CalanderPopupSheetView(day: date)
             }
     }
 
