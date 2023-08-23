6135b464801e9e12dbb0bcbc02d045a1533fa380
TobinPalmer <tobinkpalmer@gmail.com>
2023-08-23 13:37:43 -0700

    graphs working!

--- Endura/Views/Activity/ActivityView.swift
+++ Endura/Views/Activity/ActivityView.swift
@@ -32,23 +32,7 @@ struct ActivityView: View {
                         ActivityGridStats(activityData: ActivityDataWithRoute.withGridStats(activityData)(), topSpace: !activityData.data.routeData.isEmpty)
 
                         VStack {
-                            let cadenceGraph = activityData.getGraph(for: .cadence)
-                            let elevationGraph = activityData.getGraph(for: .elevation)
-                            let groundContactTimeGraph = activityData.getGraph(for: .groundContactTime)
-                            let heartRateGraph = activityData.getGraph(for: .heartRate)
-                            let paceGraph = activityData.getGraph(for: .pace)
-                            let powerGraph = activityData.getGraph(for: .power)
-                            let strideLengthGraph = activityData.getGraph(for: .strideLength)
-                            let verticalOscillationGraph = activityData.getGraph(for: .verticleOscillation)
-
-                            LineGraph(data: paceGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.convertMpsToMpm, style: PaceLineGraphStyle())
-                            LineGraph(data: heartRateGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: HeartRateLineGraphStyle())
-                            LineGraph(data: elevationGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: ElevationLineGraphStyle())
-                            LineGraph(data: cadenceGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: CadenceLineGraphStyle())
-                            LineGraph(data: powerGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: PowerLineGraphStyle())
-                            LineGraph(data: groundContactTimeGraph, step: activityData.data.graphInterval, height: 200, style: GroundContactTimeLineGraphStyle())
-                            LineGraph(data: strideLengthGraph, step: activityData.data.graphInterval, height: 200, style: StrideLengthLineGraphStyle())
-                            LineGraph(data: verticalOscillationGraph, step: activityData.data.graphInterval, height: 200, style: VerticalOscillationLineGraphStyle())
+                            ActivityGraphsView(activityData)
                         }
                     }
                     .environmentObject(activityViewModel)
