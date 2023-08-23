e7ea41d7a2baeb715b4ea9ef632e6dd9abe2dec2
TobinPalmer <tobinkpalmer@gmail.com>
2023-08-23 15:23:05 -0700

    improved graphs by spliting them into two

--- Endura/Views/Components/Activity/Stats/ActivityGraphs.swift
+++ Endura/Views/Components/Activity/Stats/ActivityGraphs.swift
@@ -2,6 +2,52 @@ import Charts
 import Foundation
 import SwiftUI
 
+@available(iOS 16.0, *)
+struct HoverableChart: View {
+    private let graph: LineGraphData
+    private let color: Color
+    @Binding private var xPosition: Int
+
+    public init(graph: LineGraphData, xPosition: Binding<Int>, color: Color) {
+        self.graph = graph
+        _xPosition = xPosition
+        self.color = color
+    }
+
+    public var body: some View {
+        Chart {
+            ForEach(graph, id: \.0) { tuple in
+                LineMark(
+                    x: .value("X values", tuple.0),
+                    y: .value("Y values", tuple.1),
+                    series: .value("Series", "Pace")
+                ).foregroundStyle(color).interpolationMethod(.catmullRom)
+            }
+        }
+        .gesture(DragGesture(minimumDistance: 10)
+            .onChanged { value in
+                let x = value.location.x
+                let max = graph.count - 1
+                let width = UIScreen.main.bounds.width - 20
+                let index = Int((x / width) * CGFloat(max))
+                xPosition = index
+            }
+        )
+        .chartOverlay { _ in
+            VStack(spacing: 0) {
+                if let pace = graph[safe: xPosition]?.1 {
+                    Text("\(pace.rounded(toPlaces: 2))")
+                        .font(.system(size: 10))
+                        .padding()
+                        .foregroundColor(.red)
+                        .background(.gray.opacity(0.5))
+                }
+            }
+            .border(.red)
+        }
+    }
+}
+
 struct ActivityGraphsView: View {
     private let activityData: ActivityDataWithRoute
 
@@ -13,60 +59,15 @@ struct ActivityGraphsView: View {
 
     public var body: some View {
         VStack {
-            let paceGraph = activityData.getGraph(for: .pace) // [(x, y)]
-            let cadenceGraph = activityData.getGraph(for: .cadence) // [(x, y)]
+            let paceGraph = activityData.getGraph(for: .pace)
+            let cadenceGraph = activityData.getGraph(for: .cadence)
 
             if #available(iOS 16.0, *) {
-                Chart {
-                    ForEach(paceGraph, id: \.0) { tuple in
-                        LineMark(
-                            x: .value("X values", tuple.0),
-                            y: .value("Y values", tuple.1),
-                            series: .value("Series", "Pace")
-                        ).foregroundStyle(.blue).interpolationMethod(.catmullRom)
-                    }
-
-                    ForEach(cadenceGraph, id: \.0) { tuple in
-                        LineMark(
-                            x: .value("X values", tuple.0),
-                            y: .value("Y values", tuple.1),
-                            series: .value("Series", "Cadence")
-                        ).foregroundStyle(.green).interpolationMethod(.catmullRom)
-                    }
-                }
-
-//        Chart(combined, id: \.1) { tuple in
-//          LineMark(
-//            x: .value("X values", tuple.1),
-//            y: .value("Y values", tuple.2)
-//          )
-//            .foregroundStyle(
-//              tuple.0 == "pace" ? .red : .blue
-//            )
-//        }
-//          .gesture(DragGesture(minimumDistance: 10)
-//            .onChanged { value in
-//              let x = value.location.x
-//              let max = paceGraph.count - 1
-//              let width = UIScreen.main.bounds.width - 20
-//              let index = Int((x / width) * CGFloat(max))
-//              xPosition = index
-//            }
-//          )
-//          .frame(height: 300)
-//          .chartOverlay { _ in
-//            VStack(spacing: 0) {
-//              if let pace = paceGraph[safe: xPosition]?.1 {
-//                Text("\(pace.rounded(toPlaces: 2))")
-//                  .font(.system(size: 10))
-//                  .padding()
-//                  .foregroundColor(.red)
-//                  .background(.gray.opacity(0.5))
-//              }
-//            }
-//              .border(.red)
-//          }
-//        Text("\(xPosition)")
+                HoverableChart(graph: paceGraph, xPosition: $xPosition, color: .blue)
+                HoverableChart(graph: cadenceGraph, xPosition: $xPosition, color: .red)
+            } else {
+                LineGraph(data: paceGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.convertMpsToMpm, style: PaceLineGraphStyle())
+                LineGraph(data: cadenceGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: CadenceLineGraphStyle())
             }
         }
     }
