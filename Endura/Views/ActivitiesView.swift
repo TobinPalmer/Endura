f1c97f9b8bfd88c31a897448d9571e6b85f8000e
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-08-23 14:30:07 -0700

    Started activities refresh

--- Endura/Views/ActivitiesView.swift
+++ Endura/Views/ActivitiesView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 @MainActor private final class ActivitiesViewModel: ObservableObject {
     @Published fileprivate var activities: [String: ActivityData] = [:]
     @Published var lastDocument: DocumentSnapshot? = nil
+    @Published private var lastRefresh = Date()
 
     private static let loadAmount = 5
 
     fileprivate func loadActivities() {
         let baseQuery = Firestore.firestore().collection("activities").order(by: "time", descending: true).limit(to: ActivitiesViewModel.loadAmount)
 
-        let query = (lastDocument == nil) ? baseQuery.whereField("time", isLessThan: Date()) : baseQuery.start(afterDocument: lastDocument!)
+        let query = (lastDocument == nil) ? baseQuery.whereField("time", isLessThan: lastRefresh) : baseQuery.start(afterDocument: lastDocument!)
 
         query.addSnapshotListener { querySnapshot, error in
             guard let snapshot = querySnapshot else {
@@ -24,36 +25,59 @@ import SwiftUI
             }
 
             snapshot.documentChanges.forEach { diff in
-                do {
-                    let data = try diff.document.data(as: ActivityData.self)
-
-                    let activity = ActivityData(
-                        averagePower: data.averagePower,
-                        calories: data.calories,
-                        comments: data.comments,
-                        distance: data.distance,
-                        description: data.description,
-                        duration: data.duration,
-                        likes: data.likes,
-                        startCity: data.startCity,
-                        startLocation: data.startLocation,
-                        time: data.time,
-                        title: data.title,
-                        totalDuration: data.totalDuration,
-                        uid: data.uid
-                    )
-
-                    if diff.type == .added || diff.type == .modified {
-                        self.activities.updateValue(activity, forKey: diff.document.documentID)
-                    } else if diff.type == .removed {
-                        self.activities.removeValue(forKey: diff.document.documentID)
-                    }
-                } catch {
-                    print("Error decoding activity: \(error)")
-                }
+                self.handleActivityDocument(diff: diff)
             }
 
             self.lastDocument = snapshot.documents.last
+        }
+    }
+
+    fileprivate func loadNewActivities() {
+        print("last refresh", lastRefresh)
+
+        let query = Firestore.firestore().collection("activities").order(by: "time", descending: true).whereField("time", isGreaterThan: lastRefresh).whereField("time", isLessThan: Date())
+
+        lastRefresh = Date()
+
+        query.addSnapshotListener { querySnapshot, error in
+            guard let snapshot = querySnapshot else {
+                print("Error fetching documents: \(error!)")
+                return
+            }
+
+            snapshot.documentChanges.forEach { diff in
+                self.handleActivityDocument(diff: diff)
+            }
+        }
+    }
+
+    private func handleActivityDocument(diff: DocumentChange) {
+        do {
+            let data = try diff.document.data(as: ActivityData.self)
+
+            let activity = ActivityData(
+                averagePower: data.averagePower,
+                calories: data.calories,
+                comments: data.comments,
+                distance: data.distance,
+                description: data.description,
+                duration: data.duration,
+                likes: data.likes,
+                startCity: data.startCity,
+                startLocation: data.startLocation,
+                time: data.time,
+                title: data.title,
+                totalDuration: data.totalDuration,
+                uid: data.uid
+            )
+
+            if diff.type == .added || diff.type == .modified {
+                self.activities.updateValue(activity, forKey: diff.document.documentID)
+            } else if diff.type == .removed {
+                self.activities.removeValue(forKey: diff.document.documentID)
+            }
+        } catch {
+            print("Error decoding activity: \(error)")
         }
     }
 
@@ -81,36 +105,37 @@ struct ActivitiesView: View {
                             }
                         }
                     }
-                    .padding(10)
+                        .padding(10)
                 } else {
                     Text("No activities")
                 }
             }
-            .refreshable {
-                // TODO: Reload the activities (Including map images)
-                print("Refreshing")
-            }
+                .refreshable {
+                    // TODO: Reload the activities (Including map images)
+                    print("Refreshing")
+                    activityViewModel.loadNewActivities()
+                }
         }
-        .enableInjection()
-        .background(Color(.secondarySystemBackground))
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                NavigationLink(destination: NewActivityView()) {
-                    Image(systemName: "plus")
+            .enableInjection()
+            .background(Color(.secondarySystemBackground))
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    NavigationLink(destination: NewActivityView()) {
+                        Image(systemName: "plus")
+                    }
                 }
-            }
-            ToolbarItem(placement: .navigationBarLeading) {
-                NavigationLink(destination: FindUsersView()) {
-                    Image(systemName: "person.2")
+                ToolbarItem(placement: .navigationBarLeading) {
+                    NavigationLink(destination: FindUsersView()) {
+                        Image(systemName: "person.2")
+                    }
                 }
-            }
 
-            ToolbarItem(placement: .navigationBarTrailing) {
-                UserProfileLink(AuthUtils.getCurrentUID()) {
-                    ProfileImage(AuthUtils.getCurrentUID(), size: 30)
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    UserProfileLink(AuthUtils.getCurrentUID()) {
+                        ProfileImage(AuthUtils.getCurrentUID(), size: 30)
+                    }
                 }
             }
-        }
     }
 }
 
@@ -120,10 +145,10 @@ class ActivitiesView_Previews: PreviewProvider {
     }
 
     #if DEBUG
-        @objc class func injected() {
-            let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-            windowScene?.windows.first?.rootViewController =
-                UIHostingController(rootView: InjectedContentView())
-        }
+    @objc class func injected() {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        windowScene?.windows.first?.rootViewController =
+            UIHostingController(rootView: InjectedContentView())
+    }
     #endif
 }
