4b18f8e5575b3c7067dd21ccb5d8e8d6a4e827e6
TobinPalmer <tobinkpalmer@gmail.com>
2023-08-26 09:41:30 -0700

    Added new testing upload view

--- Endura/Views/Components/Activity/Stats/ActivityGraphs.swift
+++ Endura/Views/Components/Activity/Stats/ActivityGraphs.swift
@@ -4,8 +4,9 @@ import SwiftUI
 
 @available(iOS 16.0, *)
 struct HoverableChart: View {
-    @EnvironmentObject var activityViewModel: ActivityViewModel
+  @EnvironmentObject var activityViewModel: ActivityViewModel
 
+<<<<<<< Updated upstream
     private let graph: LineGraphData
     private let color: Color
     private let label: String
@@ -13,7 +14,25 @@ struct HoverableChart: View {
     private let workoutStart: Date
     private let workoutEnd: Date
     @Binding private var timestamp: Date?
+||||||| Stash base
+    private let graph: LineGraphData
+    private let color: Color
+    private let label: String
+    private let valueModifier: (Double) -> String
+    private let workoutStart: Date
+    private let workoutEnd: Date
+    @Binding private var xPosition: Int
+=======
+  private let graph: LineGraphData
+  private let color: Color
+  private let label: String
+  private let valueModifier: (Double) -> String
+  private let workoutStart: Date
+  private let workoutEnd: Date
+  @Binding private var xPosition: Int
+>>>>>>> Stashed changes
 
+<<<<<<< Updated upstream
     public init(workoutStart: Date, workoutEnd: Date, graph: LineGraphData, timestamp: Binding<Date?>, color: Color, valueModifier: @escaping (Double) -> String = {
         String($0)
     }, label: String) {
@@ -25,21 +44,75 @@ struct HoverableChart: View {
         self.label = label
         self.valueModifier = valueModifier
     }
+||||||| Stash base
+    public init(workoutStart: Date, workoutEnd: Date, graph: LineGraphData, xPosition: Binding<Int>, color: Color, valueModifier: @escaping (Double) -> String = {
+        String($0)
+    }, label: String) {
+        self.workoutStart = workoutStart
+        self.workoutEnd = workoutEnd
+        self.graph = graph
+        _xPosition = xPosition
+        self.color = color
+        self.label = label
+        self.valueModifier = valueModifier
+    }
+=======
+  public init(workoutStart: Date, workoutEnd: Date, graph: LineGraphData, xPosition: Binding<Int>, color: Color, valueModifier: @escaping (Double) -> String = {
+    String($0)
+  }, label: String) {
+    self.workoutStart = workoutStart
+    self.workoutEnd = workoutEnd
+    self.graph = graph
+    _xPosition = xPosition
+    self.color = color
+    self.label = label
+    self.valueModifier = valueModifier
+  }
+>>>>>>> Stashed changes
 
-    public var body: some View {
-        if !graph.filter({ $0.1 != 0 }).isEmpty {
-            Text(label)
+  public var body: some View {
+    if !graph.filter({ $0.1 != 0 }).isEmpty {
+      Text(label)
+        .font(.title3)
+        .padding()
+      Chart {
+        ForEach(graph, id: \.0) { tuple in
+          LineMark(
+            x: .value("X values", tuple.0),
+            y: .value("Y values", tuple.1),
+            series: .value("Series", "Pace")
+          ).foregroundStyle(color).interpolationMethod(.catmullRom)
+        }
+      }
+        .frame(width: UIScreen.main.bounds.width - 20, height: 200)
+        .border(Color.gray)
+        .gesture(DragGesture(minimumDistance: 10)
+          .onChanged { value in
+            activityViewModel.analysisPosition = graph[safe: xPosition]?.0
+            activityViewModel.workoutStartDate = workoutStart
+            activityViewModel.workoutEndDate = workoutEnd
+            activityViewModel.workoutDuration = workoutEnd.timeIntervalSince(workoutStart)
+
+            let x = value.location.x
+            let max = graph.count - 1
+            let width = UIScreen.main.bounds.width - 20
+            let index = Int((x / width) * CGFloat(max))
+            xPosition = index
+          }
+        )
+        .chartOverlay { _ in
+          let _ = print("Changed xposition to \(xPosition)")
+          VStack(spacing: 0) {
+            if let pace = graph[safe: xPosition]?.1 {
+              Text("\(valueModifier(pace)) at \(graph[safe: xPosition]?.0.formatted() ?? "")")
                 .font(.title3)
+                .fontWeight(.semibold)
                 .padding()
-            Chart {
-                ForEach(graph, id: \.0) { tuple in
-                    LineMark(
-                        x: .value("X values", tuple.0),
-                        y: .value("Y values", tuple.1),
-                        series: .value("Series", "Pace")
-                    ).foregroundStyle(color).interpolationMethod(.catmullRom)
-                }
+                .foregroundColor(.white)
+                .background(color)
+                .cornerRadius(5)
             }
+<<<<<<< Updated upstream
                 .frame(width: UIScreen.main.bounds.width - 20, height: 200)
                 .border(Color.gray)
                 .gesture(DragGesture(minimumDistance: 10)
@@ -48,7 +121,21 @@ struct HoverableChart: View {
                         activityViewModel.workoutStartDate = workoutStart
                         activityViewModel.workoutEndDate = workoutEnd
                         activityViewModel.workoutDuration = workoutEnd.timeIntervalSince(workoutStart)
+||||||| Stash base
+            .frame(width: UIScreen.main.bounds.width - 20, height: 200)
+            .border(Color.gray)
+            .gesture(DragGesture(minimumDistance: 10)
+                .onChanged { value in
+                    activityViewModel.analysisPosition = graph[safe: xPosition]?.0
+                    activityViewModel.workoutStartDate = workoutStart
+                    activityViewModel.workoutEndDate = workoutEnd
+                    activityViewModel.workoutDuration = workoutEnd.timeIntervalSince(workoutStart)
+=======
+          }
+            .position(x: CGFloat(xPosition) / CGFloat(max(1, graph.count - 1)) * UIScreen.main.bounds.width, y: 100)
+>>>>>>> Stashed changes
 
+<<<<<<< Updated upstream
                         let x = value.location.x
                         let max = graph.count - 1
                         let width = UIScreen.main.bounds.width - 20
@@ -77,19 +164,63 @@ struct HoverableChart: View {
                         .frame(width: 1, height: 200)
 //                        .position(x: (CGFloat(timestamp) / CGFloat(max(1, graph.count - 1)) * UIScreen.main.bounds.width) - 12, y: 0)
                 }
+||||||| Stash base
+                    let x = value.location.x
+                    let max = graph.count - 1
+                    let width = UIScreen.main.bounds.width - 20
+                    let index = Int((x / width) * CGFloat(max)) + 3
+                    xPosition = index
+                }
+            )
+            .chartOverlay { _ in
+                let _ = print("Changed xposition to \(xPosition)")
+                VStack(spacing: 0) {
+                    if let pace = graph[safe: xPosition]?.1 {
+                        Text("\(valueModifier(pace)) at \(graph[safe: xPosition]?.0.formatted() ?? "")")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(color)
+                            .cornerRadius(5)
+                    }
+                }
+                .position(x: CGFloat(xPosition) / CGFloat(max(1, graph.count - 1)) * UIScreen.main.bounds.width, y: 100)
+
+                // Line to show where the user is
+                Rectangle()
+                    .stroke(Color.red, style: StrokeStyle(lineWidth: 1, dash: [5]))
+                    .frame(width: 1, height: 200)
+                    .position(x: (CGFloat(xPosition) / CGFloat(max(1, graph.count - 1)) * UIScreen.main.bounds.width) - 12, y: 0)
+            }
+=======
+          // Line to show where the user is
+          Rectangle()
+            .stroke(Color.red, style: StrokeStyle(lineWidth: 1, dash: [5]))
+            .frame(width: 1, height: 200)
+            .position(x: (CGFloat(xPosition) / CGFloat(max(1, graph.count - 1)) * UIScreen.main.bounds.width) - 12, y: 0)
+>>>>>>> Stashed changes
         }
     }
+  }
 }
 
 struct ActivityGraphsView: View {
-    private let activityData: ActivityDataWithRoute
+  private let activityData: ActivityDataWithRoute
 
+<<<<<<< Updated upstream
     @State private var timestamp: Date?
+||||||| Stash base
+    @State private var xPosition = 0
+=======
+  @State private var xPosition = 0
+>>>>>>> Stashed changes
 
-    init(_ activityData: ActivityDataWithRoute) {
-        self.activityData = activityData
-    }
+  init(_ activityData: ActivityDataWithRoute) {
+    self.activityData = activityData
+  }
 
+<<<<<<< Updated upstream
     public var body: some View {
         VStack {
 //            let paceGraph = activityData.getGraph(for: .pace)
@@ -109,13 +240,35 @@ struct ActivityGraphsView: View {
 //            let _ = print("Power length: \(graphData.power.count)")
 //            let _ = print("Stride length length: \(graphData.strideLength.count)")
 //            let _ = print("Vertical oscillation length: \(graphData.verticalOscillation.count)")
+||||||| Stash base
+    public var body: some View {
+        VStack {
+            let paceGraph = activityData.getGraph(for: .pace)
+            let cadenceGraph = activityData.getGraph(for: .cadence)
+            let elevationGraph = activityData.getGraph(for: .elevation)
+            let heartRateGraph = activityData.getGraph(for: .heartRate)
+            let powerGraph = activityData.getGraph(for: .power)
+            let strideLengthGraph = activityData.getGraph(for: .strideLength)
+            let verticleOscillation = activityData.getGraph(for: .verticleOscillation)
+=======
+  public var body: some View {
+    VStack {
+      let paceGraph = activityData.getGraph(for: .pace)
+      let cadenceGraph = activityData.getGraph(for: .cadence)
+      let elevationGraph = activityData.getGraph(for: .elevation)
+      let heartRateGraph = activityData.getGraph(for: .heartRate)
+      let powerGraph = activityData.getGraph(for: .power)
+      let strideLengthGraph = activityData.getGraph(for: .strideLength)
+      let verticleOscillation = activityData.getGraph(for: .verticleOscillation)
+>>>>>>> Stashed changes
 
-            if #available(iOS 16.0, *) {
+      if #available(iOS 16.0, *) {
 //        HoverableChart(graph: paceGraph, xPosition: $xPosition, color: .blue, valueModifier: ConversionUtils.convertMpsToMpm, label: "Pace")
 //        HoverableChart(graph: cadenceGraph, xPosition: $xPosition, color: .red, valueModifier: ConversionUtils.round, label: "Cadence")
 //        HoverableChart(graph: elevationGraph, xPosition: $xPosition, color: .green, valueModifier: ConversionUtils.round, label: "Elevation")
 //        HoverableChart(graph: powerGraph, xPosition: $xPosition, color: .purple, valueModifier: ConversionUtils.round, label: "Power")
 //        HoverableChart(graph: strideLengthGraph, xPosition: $xPosition, color: .yellow, valueModifier: ConversionUtils.round, label: "Stride Length")
+<<<<<<< Updated upstream
 //        HoverableChart(graph: verticalOscillation, xPosition: $xPosition, color: .pink, valueModifier: ConversionUtils.round, label: "Vertical Oscillation")
                 HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: graphData.pace, timestamp: $timestamp, color: .blue, valueModifier: ConversionUtils.convertMpsToMpm, label: "Pace")
                 HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: graphData.cadence, timestamp: $timestamp, color: .red, valueModifier: ConversionUtils.round, label: "Cadence")
@@ -133,5 +286,42 @@ struct ActivityGraphsView: View {
                 LineGraph(data: graphData.verticalOscillation, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: VerticalOscillationLineGraphStyle())
             }
         }
+||||||| Stash base
+//        HoverableChart(graph: verticleOscillation, xPosition: $xPosition, color: .pink, valueModifier: ConversionUtils.round, label: "Verticle Oscillation")
+                HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: paceGraph, xPosition: $xPosition, color: .blue, valueModifier: ConversionUtils.convertMpsToMpm, label: "Pace")
+                HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: cadenceGraph, xPosition: $xPosition, color: .red, valueModifier: ConversionUtils.round, label: "Cadence")
+                HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: elevationGraph, xPosition: $xPosition, color: .green, valueModifier: ConversionUtils.round, label: "Elevation")
+                HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: powerGraph, xPosition: $xPosition, color: .purple, valueModifier: ConversionUtils.round, label: "Power")
+                HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: strideLengthGraph, xPosition: $xPosition, color: .yellow, valueModifier: ConversionUtils.round, label: "Stride Length")
+                HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: verticleOscillation, xPosition: $xPosition, color: .pink, valueModifier: ConversionUtils.round, label: "Verticle Oscillation")
+            } else {
+                LineGraph(data: paceGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.convertMpsToMpm, style: PaceLineGraphStyle())
+                LineGraph(data: cadenceGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: CadenceLineGraphStyle())
+                LineGraph(data: elevationGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: ElevationLineGraphStyle())
+                LineGraph(data: heartRateGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: HeartRateLineGraphStyle())
+                LineGraph(data: powerGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: PowerLineGraphStyle())
+                LineGraph(data: strideLengthGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: StrideLengthLineGraphStyle())
+                LineGraph(data: verticleOscillation, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: VerticleOscillationLineGraphStyle())
+            }
+        }
+=======
+//        HoverableChart(graph: verticleOscillation, xPosition: $xPosition, color: .pink, valueModifier: ConversionUtils.round, label: "Verticle Oscillation")
+        HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: paceGraph, xPosition: $xPosition, color: .blue, valueModifier: ConversionUtils.convertMpsToMpm, label: "Pace")
+        HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: cadenceGraph, xPosition: $xPosition, color: .red, valueModifier: ConversionUtils.round, label: "Cadence")
+        HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: elevationGraph, xPosition: $xPosition, color: .green, valueModifier: ConversionUtils.round, label: "Elevation")
+        HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: powerGraph, xPosition: $xPosition, color: .purple, valueModifier: ConversionUtils.round, label: "Power")
+        HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: strideLengthGraph, xPosition: $xPosition, color: .yellow, valueModifier: ConversionUtils.round, label: "Stride Length")
+        HoverableChart(workoutStart: activityData.workoutStart, workoutEnd: activityData.workoutStart + activityData.duration, graph: verticleOscillation, xPosition: $xPosition, color: .pink, valueModifier: ConversionUtils.round, label: "Verticle Oscillation")
+      } else {
+        LineGraph(data: paceGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.convertMpsToMpm, style: PaceLineGraphStyle())
+        LineGraph(data: cadenceGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: CadenceLineGraphStyle())
+        LineGraph(data: elevationGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: ElevationLineGraphStyle())
+        LineGraph(data: heartRateGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: HeartRateLineGraphStyle())
+        LineGraph(data: powerGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: PowerLineGraphStyle())
+        LineGraph(data: strideLengthGraph, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: StrideLengthLineGraphStyle())
+        LineGraph(data: verticleOscillation, step: activityData.data.graphInterval, height: 200, valueModifier: ConversionUtils.round, style: VerticleOscillationLineGraphStyle())
+      }
+>>>>>>> Stashed changes
     }
+  }
 }
--- Endura/Views/NewActivity/NewActivityView.swift
+++ Endura/Views/NewActivity/NewActivityView.swift
@@ -3,72 +3,72 @@ import HealthKit
 import SwiftUI
 
 @MainActor final class UploadsViewModel: ObservableObject {
-    @Published fileprivate final var uploads: [HKWorkout?] = []
-    private var offset: Int = 0
+  @Published fileprivate final var uploads: [HKWorkout?] = []
+  private var offset: Int = 0
 
-    fileprivate final func activityToIcon(activityName: String) -> String {
-        switch activityName {
-        case "Running":
-            return "figure.run"
-        case "Cycling":
-            return "figure.outdoor.cycle"
-        case "Walking":
-            return "figure.walk"
-        case "Swimming":
-            return "figure.pool.swim"
-        case "Elliptical":
-            return "figure.walk"
-        case "Other":
-            return "figure.walk"
-        default:
-            return "figure.walk"
-        }
+  fileprivate final func activityToIcon(activityName: String) -> String {
+    switch activityName {
+    case "Running":
+      return "figure.run"
+    case "Cycling":
+      return "figure.outdoor.cycle"
+    case "Walking":
+      return "figure.walk"
+    case "Swimming":
+      return "figure.pool.swim"
+    case "Elliptical":
+      return "figure.walk"
+    case "Other":
+      return "figure.walk"
+    default:
+      return "figure.walk"
+    }
+  }
+
+  fileprivate final func getActivities(_ limitTo: Int) async {
+    guard limitTo > 0 else {
+      return
     }
 
-    fileprivate final func getActivities(_ limitTo: Int) async {
-        guard limitTo > 0 else {
-            return
-        }
-
-        guard uploads.count > limitTo else {
-            do {
-                let workouts = try await HealthKitUtils.getListOfWorkouts(limitTo: limitTo, offset: offset)
-                uploads.append(contentsOf: workouts)
-                offset += workouts.count
-            } catch {
-                print("Error: \(error)")
-            }
-            return
-        }
+    guard uploads.count > limitTo else {
+      do {
+        let workouts = try await HealthKitUtils.getListOfWorkouts(limitTo: limitTo, offset: offset)
+        uploads.append(contentsOf: workouts)
+        offset += workouts.count
+      } catch {
+        print("Error: \(error)")
+      }
+      return
     }
+  }
 }
 
 struct NewActivityView: View {
-    @EnvironmentObject private var navigation: NavigationModel
-    @ObservedObject private var uploadsViewModel = UploadsViewModel()
-    @State private var totalItemsLoaded: Int = 0
-    @State private var activityEndDatesToUUIDs: [Date: UUID] = [:]
+  @EnvironmentObject private var navigation: NavigationModel
+  @ObservedObject private var uploadsViewModel = UploadsViewModel()
+  @State private var totalItemsLoaded: Int = 0
+  @State private var activityEndDatesToUUIDs: [Date: UUID] = [:]
 
-    var body: some View {
-        List(uploadsViewModel.uploads, id: \.self) { activity in
-            if let activity {
-                NavigationLink(destination: PreviewWorkoutView(workout: activity)) {
-                    let workoutType = activity.workoutActivityType.name
-                    let workoutDistance = (activity.totalDistance?.doubleValue(for: .mile()) ?? 0.0).rounded(toPlaces: 2).removeTrailingZeros()
-                    HStack {
-                        if ActivityUtils.isActivityUploaded(activity) {
-                            Image(systemName: "checkmark").font(.largeTitle).foregroundColor(.green)
-                        } else {
-                            Image(systemName: uploadsViewModel.activityToIcon(activityName: workoutType)).font(.largeTitle)
-                        }
-                        Text(activity.startDate, style: .date)
-                        Text("• \(workoutDistance) mi")
-                    }
-                }
+  var body: some View {
+    List(uploadsViewModel.uploads, id: \.self) { activity in
+      if let activity {
+        NavigationLink(destination: UploadWorkoutView2(workout: activity)) {
+          let workoutType = activity.workoutActivityType.name
+          let workoutDistance = (activity.totalDistance?.doubleValue(for: .mile()) ?? 0.0).rounded(toPlaces: 2).removeTrailingZeros()
+          HStack {
+            if ActivityUtils.isActivityUploaded(activity) {
+              Image(systemName: "checkmark").font(.largeTitle).foregroundColor(.green)
+            } else {
+              Image(systemName: uploadsViewModel.activityToIcon(activityName: workoutType)).font(.largeTitle)
             }
+            Text(activity.startDate, style: .date)
+            Text("• \(workoutDistance) mi")
+          }
         }
-        .task {
-            await uploadsViewModel.getActivities(100)
-        }
+      }
     }
+      .task {
+        await uploadsViewModel.getActivities(100)
+      }
+  }
 }
