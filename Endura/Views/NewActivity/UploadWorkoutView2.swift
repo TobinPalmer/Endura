02acbeea169847f2d34e89183b1af4a1b044f037
TobinPalmer <tobinkpalmer@gmail.com>
2023-08-27 22:07:52 -0700

    started minimal workout view

--- Endura/Views/NewActivity/UploadWorkoutView2.swift
+++ Endura/Views/NewActivity/UploadWorkoutView2.swift
@@ -8,9 +8,6 @@ import SwiftUI
     @Published fileprivate var geometryRef: GeometryProxy?
     @Published fileprivate var enduraWorkout: ActivityDataWithRoute?
     @Published fileprivate var workoutStats: ActivityGridStatsData?
-    @Published fileprivate var workoutHeader: ActivityHeaderData?
-    @Published fileprivate var activityTitle: String = ""
-    @Published fileprivate var activityDescription: String = ""
 
     fileprivate final func getEnduraWorkout(_ workout: HKWorkout) async throws -> ActivityDataWithRoute {
         do {
@@ -29,10 +26,6 @@ struct PreviewWorkoutView2: View {
         previewWorkoutModel.workoutStats = HealthKitUtils.getWorkoutGridStatsData(for: workout)
     }
 
-    @MainActor func updateWorkoutHeader(_ workout: HKWorkout) async throws {
-        previewWorkoutModel.workoutHeader = try await HealthKitUtils.getWorkoutHeaderData(for: workout)
-    }
-
     @MainActor func updateEnduraWorkout(_ workout: HKWorkout) async throws {
         previewWorkoutModel.enduraWorkout = try await previewWorkoutModel.getEnduraWorkout(workout)
     }
@@ -46,26 +39,9 @@ struct PreviewWorkoutView2: View {
     var body: some View {
         VStack {
             ScrollView {
-                if let workoutHeader = previewWorkoutModel.workoutHeader {
-                    ActivityHeader(uid: workoutHeader.uid, activityData: workoutHeader)
-                } else {
-                    ActivityHeader(uid: "", activityData: nil, placeholder: true)
-                }
-
-                TextField("Title", text: $previewWorkoutModel.activityTitle)
-                    .font(.title)
-                    .textFieldStyle(EnduraTextFieldStyle())
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom, 10)
-
-                TextField("Description", text: $previewWorkoutModel.activityDescription)
-                    .font(.body)
-                    .textFieldStyle(EnduraTextFieldStyle())
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom, 10)
-
                 if var activityData = previewWorkoutModel.enduraWorkout {
                     let activityViewModel = ActivityViewModel(activityData: activityData.getIndexedGraphData(), routeLocationData: activityData.getIndexedRouteLocationData(), interval: activityData.data.graphInterval)
+
                     if !activityData.data.routeData.isEmpty {
                         VStack {
                             GeometryReader { geometry in
@@ -87,28 +63,20 @@ struct PreviewWorkoutView2: View {
 
                     ActivityGridStats(activityData: previewWorkoutModel.workoutStats, topSpace: !activityData.data.routeData.isEmpty)
 
-                    VStack {
-                        ActivityGraphsView(activityData).environmentObject(activityViewModel)
-                    }
-
                     Button {
                         Task {
                             do {
                                 if let mapRef = previewWorkoutModel.mapRef, let geometryRef = previewWorkoutModel.geometryRef {
-                                    if previewWorkoutModel.activityTitle.isEmpty {
-                                        activityData.title = ConversionUtils.getDefaultActivityName(time: activityData.time)
-                                    } else {
-                                        activityData.title = previewWorkoutModel.activityTitle
-                                        activityData.description = previewWorkoutModel.activityDescription
-                                    }
+                                    activityData.title = ConversionUtils.getDefaultActivityName(time: activityData.time)
 
-                                    try ActivityUtils.uploadActivity(activity: activityData, image: mapRef.takeScreenshot(origin: geometryRef.frame(in: .global).origin, size: geometryRef.size))
+//                  try ActivityUtils.uploadActivity(activity: activityData, image: mapRef.takeScreenshot(origin: geometryRef.frame(in: .global).origin, size: geometryRef.size))
                                 } else {
-                                    try ActivityUtils.uploadActivity(activity: activityData)
+//                  try ActivityUtils.uploadActivity(activity: activityData)
                                 }
 
                                 ActivityUtils.setActivityUploaded(workout)
                                 isShowingSummary = true
+                                return;
                                 print("setting model to true")
                             } catch {
                                 print("Error uploading workout: \(error)")
@@ -132,16 +100,13 @@ struct PreviewWorkoutView2: View {
                     await updateWorkoutStats(workout)
                 }
                 group.addTask {
-                    try await updateWorkoutHeader(workout)
-                }
-                group.addTask {
                     try await updateEnduraWorkout(workout)
                 }
             }
         }
         .fullScreenCover(isPresented: $isShowingSummary) {
             if let activityData = previewWorkoutModel.enduraWorkout {
-                PostUploadView(activityData: activityData)
+                PostUploadView(activityData: activityData, mapRef: $previewWorkoutModel.mapRef, geometryRef: $previewWorkoutModel.geometryRef)
             } else {
                 Text("Error uploading workout")
             }
