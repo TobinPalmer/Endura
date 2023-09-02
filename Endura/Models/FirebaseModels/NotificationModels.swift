5f3eaf2fa5b42f943978e16a02e4f35a55011993
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-09-02 08:57:50 -0700

    Started notifications

---
+++ Endura/Models/FirebaseModels/NotificationModels.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public enum NotificationType {
+    case friendRequest
+}
+
+public struct NotificationData {
+    public let uid: String
+}
---
+++ Endura/Utils/Firebase/Notifications.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public enum FirebaseNotificationUtils {
+    public static func sendNotification(to _: String, data _: NotificationData) {}
+}
