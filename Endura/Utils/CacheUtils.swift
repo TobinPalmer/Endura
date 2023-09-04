e0f60618f40c83787fd6cccaab0821307d5ed584
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-09-03 17:52:19 -0700

    Cache utils added

---
+++ Endura/Utils/CacheUtils.swift
@@ -0,0 +1,34 @@
+import CoreData
+import Foundation
+
+protocol Cacheable {
+    associatedtype T: NSManagedObject
+    func updateCache(_ cache: T)
+    static func fromCache(_ cache: T) -> Self
+}
+
+public enum CacheUtils {
+    public static func updateObject<T: NSManagedObject>(_: T.Type, update: (T) -> Void) {
+        let context = PersistenceController.shared.container.viewContext
+        let fetchRequest: NSFetchRequest<T> = (T.fetchRequest() as! NSFetchRequest<T>)
+        fetchRequest.fetchLimit = 1
+        do {
+            try update(context.fetch(fetchRequest).first ?? T(context: context))
+            try context.save()
+        } catch {
+            print("Error updating object: \(error)")
+        }
+    }
+
+    public static func fetchObject<T: NSManagedObject>(_: T.Type) -> T? {
+        let context = PersistenceController.shared.container.viewContext
+        let fetchRequest: NSFetchRequest<T> = T.fetchRequest() as! NSFetchRequest<T>
+        fetchRequest.fetchLimit = 1
+        do {
+            return try context.fetch(fetchRequest).first
+        } catch {
+            print("Error fetching object: \(error)")
+            return nil
+        }
+    }
+}
