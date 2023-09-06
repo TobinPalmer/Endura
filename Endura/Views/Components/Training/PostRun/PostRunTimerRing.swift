9acf60b25cb463943a0a7b426a08cc2abaeb16de
TobinPalmer <tobinkpalmer@gmail.com>
2023-09-05 19:09:06 -0700

    Minor changes to post run view

--- Endura/Views/Components/Training/PostRun/PostRunTimerRing.swift
+++ Endura/Views/Components/Training/PostRun/PostRunTimerRing.swift
@@ -19,7 +19,7 @@ struct PostRunTimerRing: View {
     }
 
     public var body: some View {
-        let progress = self.duration == 0 ? 0 : self.currentTime / self.duration
+        let progress = duration == 0 ? 0 : currentTime / duration
         Circle()
             .rotation(.degrees(-90))
             .trim(from: 0, to: CGFloat(progress))
@@ -31,24 +31,24 @@ struct PostRunTimerRing: View {
                 style: .init(lineWidth: lineWidth, lineCap: .round)
             )
             .overlay(
-                GeometryReader { geometry in
+                GeometryReader { _ in
                     Circle()
-                        .fill(self.gradient.stops[1].color)
-                        .frame(width: self.lineWidth, height: self.lineWidth)
-                        .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
-                        .offset(x: min(geometry.size.width, geometry.size.height) / 2)
+                        .fill(gradient.stops[1].color)
+                        .frame(width: lineWidth, height: lineWidth)
+                        //            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                        //            .offset(x: min(geometry.size.width, geometry.size.height) / 2)
                         .rotationEffect(.degrees(progress * 360 - 90))
-                        .shadow(color: .black, radius: self.lineWidth / 2, x: 0, y: 0)
+                        .shadow(color: .black, radius: lineWidth / 2, x: 0, y: 0)
                 }
                 .clipShape(
                     Circle()
                         .rotation(.degrees(-90 + progress * 360 - 0.5))
                         .trim(from: 0, to: 0.25)
-                        .stroke(style: .init(lineWidth: self.lineWidth))
+                        .stroke(style: .init(lineWidth: lineWidth))
                 )
             )
             .scaledToFit()
-            .padding(lineWidth / 2)
+            //      .padding(lineWidth / 2)
             .animation(.linear(duration: currentTime), value: currentTime)
             .frame(width: 200, height: 200)
     }
--- Endura/Views/PostRun/PostRunView.swift
+++ Endura/Views/PostRun/PostRunView.swift
@@ -24,23 +24,38 @@ private final class PostRunViewModel: ObservableObject {
 
 struct PostRunView: View {
     @StateObject private var viewModel = PostRunViewModel()
+    @State private var isDoneWithTimerExercise = false
 
     public var body: some View {
-        VStack {
-            let currentExercise = postRunEasyDay[viewModel.currentExerciseIndex]
-            Text("\(String(describing: currentExercise.type)) for \(String(describing: currentExercise.parameter))")
-            switch currentExercise.parameter {
-            case let .count(count):
-                Text("Do \(count)")
-            case let .time(time):
-                Text("Do for \(time)")
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+
+            VStack {
+                let currentExercise = postRunEasyDay[viewModel.currentExerciseIndex]
+                Text("\(String(describing: currentExercise.type)) for \(String(describing: currentExercise.parameter))")
+                switch currentExercise.parameter {
+                case let .count(count):
+                    Text("Do \(count)")
+                case let .time(time):
+                    PostRunTimerRing(duration: time, currentTime: viewModel.currentTime, lineWidth: 10, gradient: Gradient(colors: [.red, .blue]))
+
+                    let _ = DispatchQueue.main.asyncAfter(deadline: .now() + time) {
+                        isDoneWithTimerExercise = true
+                    }
+                }
+                Button("Next Exercise") {
+                    viewModel.currentExerciseIndex += 1
+                    isDoneWithTimerExercise = false
+                }
+                .disabled(!isDoneWithTimerExercise)
+
+                if viewModel.currentTime <= 0 {
+                    Button("Start") {
+                        viewModel.startTimer(duration: 10)
+                    }
+                }
             }
-            Button("Increment Index") {
-                viewModel.currentExerciseIndex += 1
-            }
-        }
-        .onAppear {
-            viewModel.startTimer(duration: 10)
         }
     }
 }
