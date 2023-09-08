60e44c14d312f0034c890910c9a3a5e2a8a10600
TobinPalmer <tobinkpalmer@gmail.com>
2023-09-08 14:21:48 -0700

    post run view clean

--- Endura/Views/Components/Training/PostRun/PostRunTimerRing.swift
+++ Endura/Views/Components/Training/PostRun/PostRunTimerRing.swift
@@ -39,10 +39,8 @@ struct PostRunTimerRing: View {
 
     var body: some View {
         let progress = progress / 10
+        let _ = Global.log.info("Change \(progress)")
         ZStack {
-//            Circle()
-//                .stroke(Color.outlineRed, lineWidth: 20)
-//                .frame(width: size, height: size)
             Circle()
                 .stroke(Color.outlineRed, lineWidth: 20)
                 .frame(width: size, height: size)
@@ -76,31 +74,8 @@ struct PostRunTimerRing: View {
                                 .frame(width: size, height: size)
                         }
                 }
-
-//            Circle()
-//                .trim(from: 0, to: progress)
-//                .stroke(
-//                    AngularGradient(
-//                        gradient: Gradient(colors: colors),
-//                        center: .center,
-//                        startAngle: .degrees(0),
-//                        endAngle: .degrees(360)
-//                    ),
-//                    style: StrokeStyle(lineWidth: 20, lineCap: .round)
-//                ).rotationEffect(.degrees(-90))
-//            Circle()
-//                .frame(width: 20, height: 20)
-//                .foregroundColor(Color.darkRed)
-//                .offset(y: -150)
-//            Circle()
-//                .frame(width: 20, height: 20)
-//                .foregroundColor(progress > 0.95 ? Color.lightRed : Color.lightRed.opacity(0))
-//                .offset(y: -150)
-//                .rotationEffect(Angle.degrees(360 * Double(progress)))
-//                .shadow(color: progress > 0.96 ? Color.black.opacity(0.1) : Color.clear, radius: 3, x: 4, y: 0)
         }
-        .animation(.linear(duration: 1.0), value: progress)
+        .animation(.linear(duration: progress == 0 ? 0 : 1.0), value: progress)
         .frame(width: size, height: size)
-        .border(.blue)
     }
 }
