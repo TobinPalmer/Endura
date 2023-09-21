b53ea8cd156ce8d4f517c8f801c06ebca15c280f
TobinPalmer <tobinkpalmer@gmail.com>
2023-09-20 18:44:58 -0700

    fixed exessive use of SwiftUI natives

--- Endura/Views/Components/Training/TrainingGoal.swift
+++ Endura/Views/Components/Training/TrainingGoal.swift
@@ -10,47 +10,47 @@ struct TrainingGoal: View {
 
     var body: some View {
         HStack {
-            HStack(spacing: 0) {
-                ZStack {
-                    switch goal {
-                    case .warmup:
-                        Image("figure_running")
-                    case .run:
-//            Image(systemName: "figure.run")
-                        Text("RUN")
-                    case .postRun:
-//            Image(systemName: "figure.cooldown")
-                        Text("COOLDOWN")
-                    }
+            ZStack {
+                switch goal {
+                case .warmup:
+                    Image(systemName: "figure.cooldown")
+                case .run:
+                    Image(systemName: "figure.run")
+                case .postRun:
+                    Image(systemName: "figure.strengthtraining.functional")
                 }
-                VStack {
-                    Text("Warm Up")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-
-                    switch goal {
-                    case let .run(dificulty, distance, time, pace):
-                        VStack {
-                            Text("\(distance.removeTrailingZeros()) Miles")
-                            Text("\(time.removeTrailingZeros()) Minutes")
-                            Text("\(pace.removeTrailingZeros()) Minutes/Mile")
-                            Text("\(dificulty.rawValue)")
-                        }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    case let .postRun(_, time, _):
-                        HStack {
-                            Text("\(time.removeTrailingZeros()) Minutes")
-                        }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    case let .warmup(time, _):
-                        HStack {
-                            Text("\(time.removeTrailingZeros()) Minutes")
-                        }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
+            VStack(alignment: .leading) {
+                switch goal {
+                case .run:
+                    Text("Run")
+                case .postRun:
+                    Text("Post Run")
+                case .warmup:
+                    Text("Warm Up")
+                }
+
+                switch goal {
+                case let .run(dificulty, distance, time, pace):
+                    VStack {
+                        Text("\(distance.removeTrailingZeros()) Miles")
+                        Text("\(time.removeTrailingZeros()) Minutes")
+                        Text("\(pace.removeTrailingZeros()) Minutes/Mile")
+                        Text("\(dificulty.rawValue)")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                case let .postRun(_, time, _):
+                    HStack {
+                        Text("\(time.removeTrailingZeros()) Minutes")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                case let .warmup(time, _):
+                    HStack {
+                        Text("\(time.removeTrailingZeros()) Minutes")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }
         }
         .padding(26)
         .EnduraDefaultBox()
