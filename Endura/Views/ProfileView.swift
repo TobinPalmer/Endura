e47962f5b0daf17856b8a3df007227faecc83906
TobinPalmer <tobinkpalmer@gmail.com>
2023-09-23 13:24:28 -0700

    added tabs to profile view

--- Endura/Views/ProfileView.swift
+++ Endura/Views/ProfileView.swift
@@ -1,10 +1,55 @@
 import Foundation
 import SwiftUI
 
+private enum ProfileViewPages {
+    case activities
+    case progress
+}
+
+private final class ProfileViewModel: ObservableObject {
+    @Published fileprivate var currentPage: ProfileViewPages = .progress
+}
+
 struct ProfileView: View {
-    var body: some View {
+    @StateObject private var viewModel = ProfileViewModel()
+
+    public var body: some View {
         VStack {
-            ProfileImage(AuthUtils.getCurrentUID(), size: 128)
+            HStack {
+                Button(action: {
+                    viewModel.currentPage = .activities
+                }, label: {
+                    Text("Activities")
+                        .font(.title2)
+                        .foregroundColor(viewModel.currentPage == .activities ? Color("TextMuted") : Color("Text"))
+                        .padding(.horizontal, 8)
+                })
+
+                Button(action: {
+                    viewModel.currentPage = .progress
+                }, label: {
+                    Text("Progress")
+                        .font(.title2)
+                        .foregroundColor(viewModel.currentPage == .progress ? Color("TextMuted") : Color("Text"))
+                        .padding(.horizontal, 8)
+                })
+            }
+
+            Divider()
+
+            if viewModel.currentPage == .activities {
+                Text("Activities")
+            } else {
+                HStack {
+                    ProfileImage(AuthUtils.getCurrentUID(), size: 128)
+                        .padding(.horizontal, 8)
+
+                    Text("Tobin Palmer")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }
+
+            Spacer()
         }
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
