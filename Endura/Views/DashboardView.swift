72ba643905bef706384598b4897df52d7357ba2e
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-09-30 19:45:03 -0700

    Cleaned up dashboard

--- Endura/Views/DashboardView.swift
+++ Endura/Views/DashboardView.swift
@@ -15,77 +15,64 @@ struct DashboardView: View {
             Color("Background")
                 .ignoresSafeArea()
 
-            Text("HI")
             ScrollView {
                 VStack {
+                    HStack {
+                        ForEach(activeUserModel.data.friends, id: \.self) { friend in
+                            UserProfileLink(friend) {
+                                ProfileImage(friend, size: 50)
+                            }
+                        }
+                        NavigationLink(destination: FindUsersView()) {
+                            Image(systemName: "plus.circle")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundStyle(Color.accentColor)
+                                .clipShape(Circle())
+                                .frame(width: 50, height: 50)
+                        }
+                        Spacer()
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+
                     VStack(alignment: .leading) {
                         DailySummaryGraph()
                     }
                     .frame(minWidth: 0, maxWidth: .infinity)
 
-                    HStack(spacing: 10) {
-                        VStack {
-                            GoalRing(.distance)
-                                .frame(maxHeight: 70)
-                        }
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                        .background(.red)
-                        .cornerRadius(8)
-
-                        VStack {
-                            GoalRing(.distance)
-                                .frame(maxHeight: 70)
-                        }
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                        .background(.orange)
-                        .cornerRadius(8)
-                    }
-                    .frame(minWidth: 0, maxWidth: .infinity)
-                }
-
-                Text(response ?? "no response yet").task {
-                    let palmClient = GenerativeLanguage(apiKey: ProcessInfo.processInfo.environment["PALM_API_KEY"]!)
-                    do {
-                        print("Sending message")
-                        let prompt = "What ai are you? Can you tell me about yourself?"
-
-                        let response = try await palmClient.generateText(with: prompt)
-                        print("Got response \(response)")
-
-                        if let candidate = response.candidates?.first, let text = candidate.output {
-                            self.response = text
-                        }
-                    } catch {
-                        print("Error with palm response: \(error)")
-                    }
+//                    HStack(spacing: 10) {
+//                        VStack {
+//                            GoalRing(.distance)
+//                                .frame(maxHeight: 70)
+//                        }
+//                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+//                            .background(.red)
+//                            .cornerRadius(8)
+//
+//                        VStack {
+//                            GoalRing(.distance)
+//                                .frame(maxHeight: 70)
+//                        }
+//                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+//                            .background(.orange)
+//                            .cornerRadius(8)
+//                    }
+//                        .frame(minWidth: 0, maxWidth: .infinity)
                 }
 
                 TrainingGoalList()
             }
         }
-        //      .padding(5)
         .toolbar {
-//            ToolbarItem(placement: .navigationBarLeading) {
-//                NavigationLink(destination: NewActivityView()) {
-//                    Image(systemName: "plus")
-//                }
-//            }
             ToolbarItem(placement: .navigationBarTrailing) {
                 NavigationLink(destination: NotificationsView()) {
-                    Image("bell")
-                        .resizable()
-                        .frame(width: 35, height: 35)
+                    Image(systemName: "bell")
                         .overlay(
                             NotificationCountView(value: $notificationsModel.unreadCount)
                         )
                 }
             }
-//        ToolbarItem(placement: .navigationBarLeading) {
-//          NavigationLink(destination: FindUsersView()) {
-//            Image(systemName: "person.2")
-//          }
-//        }
-
             ToolbarItem(placement: .navigationBarTrailing) {
                 UserProfileLink(AuthUtils.getCurrentUID()) {
                     ProfileImage(AuthUtils.getCurrentUID(), size: 30)
