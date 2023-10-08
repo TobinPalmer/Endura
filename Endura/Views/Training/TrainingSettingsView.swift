0ad7a818b11bd3c4d148314013332c039f439f98
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-08 11:50:47 -0700

    Added alert and routines to training settings

--- Endura/Views/Training/TrainingSettingsView.swift
+++ Endura/Views/Training/TrainingSettingsView.swift
@@ -3,23 +3,37 @@ import SwiftUI
 
 struct TrainingSettingsView: View {
     @EnvironmentObject var activeUser: ActiveUserModel
+    @State var showingAlert = false
 
     var body: some View {
         List {
             Section(header: Text("Day Availabilities")) {
                 ForEach(0 ..< 7) { day in
-                    HStack {
-                        Toggle("\(WeekDay(rawValue: day)!.getShortName())", isOn: Binding(
-                            get: {
-                                activeUser.settings.data.training.dayAvailabilities[day] ?? true
-                            },
-                            set: { newValue in
-                                activeUser.settings.data.training.dayAvailabilities[day] = newValue
+                    Toggle("\(WeekDay(rawValue: day)!.getShortName())", isOn: Binding(
+                        get: {
+                            activeUser.settings.data.training.dayAvailabilities[day] ?? true
+                        },
+                        set: { newValue in
+                            if activeUser.settings.data.training.dayAvailabilities.filter({ $0.value == false })
+                                .count >= 3, newValue == false
+                            {
+                                showingAlert = true
+                                return
                             }
-                        ))
-                    }
+                            activeUser.settings.data.training.dayAvailabilities[day] = newValue
+                        }
+                    ))
                 }
             }
+            NavigationLink(
+                destination: Text("Routines")
+            ) {
+                Text("Routines")
+                    .badge(4)
+            }
+        }
+        .alert(Text("You must have at least 4 days available for training"), isPresented: $showingAlert) {
+            Button("OK", role: .cancel) {}
         }
     }
 }
