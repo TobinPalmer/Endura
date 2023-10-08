1173ee09db76ae7ec521be3b09b1e57903639ff6
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-07 18:02:21 -0700

    Added upload button

--- Endura/Views/NewActivity/NewActivityView.swift
+++ Endura/Views/NewActivity/NewActivityView.swift
@@ -59,97 +59,114 @@ struct NewActivityView: View {
     @State private var selectedActivities: Set<HKWorkout> = []
 
     var body: some View {
-        if !isAuthorized {
-            Text("Please authorize Apple Health to continue.")
-            Button("Authorize") {
-                HealthKitUtils.requestAuthorization { success in
-                    isAuthorized = success
-                    print("setting isAuthorized to \(success)")
-                    print("but healthKit is \(HealthKitUtils.isAuthorized())")
-                }
-            }
-        } else {
-            let groupedActivities = Dictionary(grouping: uploadsViewModel.uploads.compactMap {
-                $0
-            }) { workout in
-                Calendar.current.startOfDay(for: workout.startDate)
-            }
-            let sortedDates = groupedActivities.keys.sorted(by: >)
-
-            if sortedDates.isEmpty {
-                VStack {
-                    ZStack {
-                        Image(systemName: "nosign")
-                            .font(.system(size: 150))
-                            .foregroundColor(Color("Text"))
-                            .zIndex(1)
-
-                        Image(systemName: "figure.walk")
-                            .font(.system(size: 100))
-                            .foregroundColor(Color("TextMuted"))
+        VStack {
+            if !isAuthorized {
+                Text("Please authorize Apple Health to continue.")
+                Button("Authorize") {
+                    HealthKitUtils.requestAuthorization { success in
+                        isAuthorized = success
+                        print("setting isAuthorized to \(success)")
+                        print("but healthKit is \(HealthKitUtils.isAuthorized())")
                     }
+                }
+            } else {
+                let groupedActivities = Dictionary(grouping: uploadsViewModel.uploads.compactMap {
+                    $0
+                }) { workout in
+                    Calendar.current.startOfDay(for: workout.startDate)
+                }
+                let sortedDates = groupedActivities.keys.sorted(by: >)
 
+                if sortedDates.isEmpty {
                     VStack {
-                        Text("No activities found.")
-                            .foregroundColor(Color("TextMuted"))
-                            .font(.title2)
+                        ZStack {
+                            Image(systemName: "nosign")
+                                .font(.system(size: 150))
+                                .foregroundColor(Color("Text"))
+                                .zIndex(1)
 
-                        Text("Check the Apple Health app to see if you have you have any workouts.")
-                            .foregroundColor(Color("TextMuted"))
-                            .font(.subheadline)
-                            .multilineTextAlignment(.center)
-                            .padding(.top, 5)
+                            Image(systemName: "figure.walk")
+                                .font(.system(size: 100))
+                                .foregroundColor(Color("TextMuted"))
+                        }
+
+                        VStack {
+                            Text("No activities found.")
+                                .foregroundColor(Color("TextMuted"))
+                                .font(.title2)
+
+                            Text("Check the Apple Health app to see if you have you have any workouts.")
+                                .foregroundColor(Color("TextMuted"))
+                                .font(.subheadline)
+                                .multilineTextAlignment(.center)
+                                .padding(.top, 5)
+                        }
+                        .padding(.horizontal, 26)
                     }
-                    .padding(.horizontal, 26)
                 }
-            }
 
-            List(selection: $selectedActivities) {
-                ForEach(sortedDates, id: \.self) { date in
-                    Section(header: Text("\(date.formatted(date: .abbreviated, time: .omitted))")) {
-                        ForEach(groupedActivities[date]!, id: \.self) { activity in
-                            NavigationLink(destination: UploadWorkoutView(workout: activity)) {
-                                let workoutType = activity.workoutActivityType.name
-                                let workoutDistance = (activity.totalDistance?.doubleValue(for: .mile()) ?? 0.0)
-                                    .rounded(toPlaces: 2).removeTrailingZeros()
-                                Label {
-                                    Text(
-                                        "\(activity.startDate.formatted(date: .omitted, time: .shortened)) • \(workoutDistance) mi"
-                                    )
-                                } icon: {
-                                    if ActivityUtils.isActivityUploaded(activity) {
-                                        Image(systemName: "checkmark").font(.title2).foregroundColor(.green)
-                                    } else {
-                                        Image(systemName: uploadsViewModel.activityToIcon(activityName: workoutType))
-                                            .font(.title2)
+                List(selection: $selectedActivities) {
+                    ForEach(sortedDates, id: \.self) { date in
+                        Section(header: Text("\(date.formatted(date: .abbreviated, time: .omitted))")) {
+                            ForEach(groupedActivities[date]!, id: \.self) { activity in
+                                NavigationLink(destination: UploadWorkoutView(workout: activity)) {
+                                    let workoutType = activity.workoutActivityType.name
+                                    let workoutDistance = (activity.totalDistance?.doubleValue(for: .mile()) ?? 0.0)
+                                        .rounded(toPlaces: 2).removeTrailingZeros()
+                                    Label {
+                                        Text(
+                                            "\(activity.startDate.formatted(date: .omitted, time: .shortened)) • \(workoutDistance) mi"
+                                        )
+                                    } icon: {
+                                        if ActivityUtils.isActivityUploaded(activity) {
+                                            Image(systemName: "checkmark").font(.title2).foregroundColor(.green)
+                                        } else {
+                                            Image(systemName: uploadsViewModel
+                                                .activityToIcon(activityName: workoutType))
+                                                .font(.title2)
+                                        }
                                     }
+                                    .frame(height: 30)
+                                    .selectionDisabled(ActivityUtils.isActivityUploaded(activity))
+                                    .tag(activity)
                                 }
-                                .frame(height: 30)
-                                .selectionDisabled(ActivityUtils.isActivityUploaded(activity))
-                                .tag(activity)
                             }
                         }
                     }
                 }
-            }
-            .onChange(of: selectedActivities) { _ in
-                if !selectionMode {
-                    self.selectedActivities = []
+                .onChange(of: selectedActivities) { _ in
+                    if !selectionMode {
+                        self.selectedActivities = []
+                    }
                 }
-            }
-            .environment(\.editMode, .constant(selectionMode ? EditMode.active : EditMode.inactive))
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(selectionMode ? "Cancel" : "Select") {
-                        withAnimation {
-                            selectionMode.toggle()
-                            selectedActivities = []
+                .environment(\.editMode, .constant(selectionMode ? EditMode.active : EditMode.inactive))
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(selectionMode ? "Cancel" : "Select") {
+                            withAnimation {
+                                selectionMode.toggle()
+                                selectedActivities = []
+                            }
                         }
                     }
                 }
+                .task {
+                    await uploadsViewModel.getActivities(100)
+                }
             }
-            .task {
-                await uploadsViewModel.getActivities(100)
+            if selectionMode {
+                Button("Upload \(selectedActivities.count) Activities") {
+                    for activity in selectedActivities {
+                        print("Uploading \(activity.workoutActivityType.name)...")
+                    }
+                    withAnimation {
+                        selectionMode.toggle()
+                        selectedActivities = []
+                    }
+                }
+                .buttonStyle(EnduraNewButtonStyle())
+                .padding(.horizontal, 16)
+                .padding(.bottom, 16)
             }
         }
     }
