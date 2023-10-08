2e16494d1d443f87cf6195cbf863b8e585b56c57
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-08 12:07:57 -0700

    Added confirmation to toggles and improved routines link

--- Endura/Views/Training/TrainingSettingsView.swift
+++ Endura/Views/Training/TrainingSettingsView.swift
@@ -3,11 +3,18 @@ import SwiftUI
 
 struct TrainingSettingsView: View {
     @EnvironmentObject var activeUser: ActiveUserModel
-    @State var showingAlert = false
+    @State var showingBadAlert = false
+    @State var showingConfirmAlert = false
+    @State var dayToRemove: Int? = nil
 
     var body: some View {
         List {
-            Section(header: Text("Day Availabilities")) {
+            Section(
+                header: Text("Day Availabilities"),
+                footer: Text(
+                    "Only unselect days you are always unavailable for training. You can always edit individual days later."
+                )
+            ) {
                 ForEach(0 ..< 7) { day in
                     Toggle("\(WeekDay(rawValue: day)!.getShortName())", isOn: Binding(
                         get: {
@@ -17,23 +24,39 @@ struct TrainingSettingsView: View {
                             if activeUser.settings.data.training.dayAvailabilities.filter({ $0.value == false })
                                 .count >= 3, newValue == false
                             {
-                                showingAlert = true
+                                showingBadAlert = true
                                 return
                             }
-                            activeUser.settings.data.training.dayAvailabilities[day] = newValue
+                            if newValue == false {
+                                showingConfirmAlert = true
+                                dayToRemove = day
+                            } else {
+                                activeUser.settings.data.training.dayAvailabilities[day] = newValue
+                            }
                         }
                     ))
                 }
             }
-            NavigationLink(
-                destination: Text("Routines")
-            ) {
-                Text("Routines")
-                    .badge(4)
+            Section(header: Text("Routines"), footer: Text("Edit your pre-run or post-run routines")) {
+                NavigationLink(
+                    destination: Text("Edit Routines")
+                ) {
+                    Text("Routines")
+                        .badge(4)
+                        .badgeProminence(.increased)
+                }
             }
         }
-        .alert(Text("You must have at least 4 days available for training"), isPresented: $showingAlert) {
+        .alert(Text("You must have at least 4 days available for training"), isPresented: $showingBadAlert) {
             Button("OK", role: .cancel) {}
+        }
+        .alert(Text("Are you sure you ALWAYS will be unavailable on this day?"),
+               isPresented: $showingConfirmAlert)
+        {
+            Button("Yes", role: .destructive) {
+                activeUser.settings.data.training.dayAvailabilities[dayToRemove!] = false
+            }
+            Button("No", role: .cancel) {}
         }
     }
 }
