d31df026a63d01fdd94900a1c004900683cf879e
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-09 18:17:23 -0700

    Improved training goal

--- Endura/Views/Components/Training/Goals/TrainingGoal.swift
+++ Endura/Views/Components/Training/Goals/TrainingGoal.swift
@@ -10,75 +10,76 @@ struct TrainingGoal: View {
 
     var body: some View {
         NavigationLink(destination: TrainingGoalDetails(goal)) {
-            HStack {
+            HStack(alignment: .top, spacing: 10) {
                 Image(systemName: goal.getIcon())
-                    .background(
-                        RoundedRectangle(cornerRadius: 50)
-                            .foregroundColor(goal.getBackgroundColor())
-                            .frame(width: 50, height: 50)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 50)
-                                    .stroke(goal.getColor(), lineWidth: 2)
-                            )
-                    )
-                    .frame(width: 50, height: 50)
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(goal.getTitle())
-                        //                        switch goal {
-                        //                        case let .run(data):
-                        //                            VStack(alignment: .leading) {
-                        //                                HStack {
-                        //                                    Text("\(data.getDistance().removeTrailingZeros()) Miles")
-                        //                                }
-                        //                            }
-                        //                        case let .routine(data):
-                        //                            VStack(alignment: .leading) {
-                        //                                Text("\(data.count) Exercises")
-                        //                            }
-                        //                        }
-                        //                    }
-                        //                    Spacer()
-                        //                    switch goal {
-                        //                    case let .routine(data):
-                        //                        HStack {
-                        //                            if data.progress.completed {
-                        //                                Image(systemName: "checkmark")
-                        //                                    .foregroundColor(.green)
-                        //                            } else {
-                        //                                Text("\(FormattingUtils.secondsToFormattedTime(data.time))
-                        //                                Minutes")
-                        //                                    .padding()
-                        //                                    .background(RoundedRectangle(cornerRadius: 50).foregroundColor(goal.getColor()))
-                        //                            }
-                        //                        }
-                        //                    case let .run(data):
-                        //                        HStack {
-                        //                            if data.progress.completed {
-                        //                                Image(systemName: "checkmark")
-                        //                                    .foregroundColor(.green)
-                        //                            } else {
-                        //                                VStack {
-                        //                                    switch data.workout {
-                        //                                    case let .distance(distance):
-                        //                                        Text("\(FormattingUtils.formatMiles(distance)) Miles")
-                        //                                    case let .time(time):
-                        //                                        Text("\(FormattingUtils.secondsToFormattedTime(time))
-                        //                                        Minutes")
-                        //                                    default: EmptyView()
-                        //                                    }
-                        //                                }
-                        //                                .padding()
-                        //                                .background(RoundedRectangle(cornerRadius: 50).foregroundColor(goal.getColor()))
-                        //                            }
-                        //                        }
-                        //                    }
-                    }
+                    .font(.title)
+                    .foregroundColor(goal.getColor())
+                    .fontWeight(.bold)
+
+                VStack(alignment: .leading) {
+                    Text(goal.getTitle())
+                        .font(.title3)
+                        .fontColor(.primary)
+                        .fontWeight(.bold)
+
+                    Text("This is the description of the goal.")
+                        .multilineTextAlignment(.leading)
+                        .font(.body)
+                        .fontColor(.secondary)
                 }
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
+            .alignFullWidth()
             .padding(26)
             .enduraDefaultBox()
+
+            //                        switch goal {
+            //                        case let .run(data):
+            //                            VStack(alignment: .leading) {
+            //                                HStack {
+            //                                    Text("\(data.getDistance().removeTrailingZeros()) Miles")
+            //                                }
+            //                            }
+            //                        case let .routine(data):
+            //                            VStack(alignment: .leading) {
+            //                                Text("\(data.count) Exercises")
+            //                            }
+            //                        }
+            //                    }
+            //                    Spacer()
+            //                    switch goal {
+            //                    case let .routine(data):
+            //                        HStack {
+            //                            if data.progress.completed {
+            //                                Image(systemName: "checkmark")
+            //                                    .foregroundColor(.green)
+            //                            } else {
+            //                                Text("\(FormattingUtils.secondsToFormattedTime(data.time))
+            //                                Minutes")
+            //                                    .padding()
+            //                                    .background(RoundedRectangle(cornerRadius: 50).foregroundColor(goal.getColor()))
+            //                            }
+            //                        }
+            //                    case let .run(data):
+            //                        HStack {
+            //                            if data.progress.completed {
+            //                                Image(systemName: "checkmark")
+            //                                    .foregroundColor(.green)
+            //                            } else {
+            //                                VStack {
+            //                                    switch data.workout {
+            //                                    case let .distance(distance):
+            //                                        Text("\(FormattingUtils.formatMiles(distance)) Miles")
+            //                                    case let .time(time):
+            //                                        Text("\(FormattingUtils.secondsToFormattedTime(time))
+            //                                        Minutes")
+            //                                    default: EmptyView()
+            //                                    }
+            //                                }
+            //                                .padding()
+            //                                .background(RoundedRectangle(cornerRadius: 50).foregroundColor(goal.getColor()))
+            //                            }
+            //                        }
+            //                    }
         }
     }
 }
