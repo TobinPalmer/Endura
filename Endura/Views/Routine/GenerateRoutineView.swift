25be68a7f5aba2bf6421b5b1df57478e545be06f
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-26 19:52:30 -0700

    Added contents and styled generate routine view

--- Endura/Views/Routine/GenerateRoutineView.swift
+++ Endura/Views/Routine/GenerateRoutineView.swift
@@ -2,8 +2,9 @@ import Foundation
 import SwiftUI
 
 private struct RoutineGenerationInfo {
-    var feeling: Feeling
-    var hurtingParts: [BodyPart]
+    var feeling: Double = 8
+    var hurtingParts: [BodyPart] = []
+    var notes: String = ""
 }
 
 struct GenerateRoutineView: View {
@@ -11,10 +12,7 @@ struct GenerateRoutineView: View {
     @Binding public var routineData: RoutineData
     @State private var routine: RoutineData
 
-    @State private var generationInfo: RoutineGenerationInfo = .init(
-        feeling: .good,
-        hurtingParts: []
-    )
+    @State private var generationInfo: RoutineGenerationInfo = .init()
 
     public init(_ routineData: Binding<RoutineData>) {
         _routineData = routineData
@@ -23,48 +21,88 @@ struct GenerateRoutineView: View {
 
     var body: some View {
         VStack {
-            Text("How do you feel now after the run?")
-                .font(.title)
-                .fontWeight(.bold)
-                .fontColor(.primary)
-
-            Picker("Feeling", selection: $generationInfo.feeling) {
-                ForEach(Feeling.allCases, id: \.self) { feeling in
-                    Text(feeling.rawValue)
-                        .foregroundColor(feeling.getColor())
-                }
-            }
-            .pickerStyle(SegmentedPickerStyle())
-
-            Text("Anything hurting or feeling sore?")
-                .font(.title)
-                .fontWeight(.bold)
-                .fontColor(.primary)
-
             ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
-                    ForEach(BodyPart.allCases, id: \.self) { bodyPart in
-                        Button {
-                            if generationInfo.hurtingParts.contains(bodyPart) {
-                                generationInfo.hurtingParts.removeAll {
-                                    $0 == bodyPart
+                Text("How are you feeling after your run?")
+                    .font(.body)
+                    .fontWeight(.bold)
+                    .fontColor(.primary)
+                    .alignFullWidth()
+
+                Text("\(Int(generationInfo.feeling))")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .padding(.top, 10)
+                    .foregroundColor(Color(
+                        hue: 0.03 * generationInfo.feeling,
+                        saturation: 1,
+                        brightness: 0.8
+                    ))
+                Slider(value: $generationInfo.feeling, in: 0 ... 9, step: 1)
+                    .accentColor(Color(
+                        hue: 0.03 * generationInfo.feeling,
+                        saturation: 1,
+                        brightness: 0.8
+                    ))
+                HStack {
+                    ForEach(
+                        0 ..< 10,
+                        id: \.self
+                    ) { index in
+                        Text("\(index + 1)")
+                            .font(.caption)
+                            .foregroundColor(Color(
+                                hue: 0.03 * Double(index),
+                                saturation: 1,
+                                brightness: 0.8
+                            ))
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                .padding(.bottom, 20)
+
+                Text("Anything hurting or feeling sore?")
+                    .font(.body)
+                    .fontWeight(.bold)
+                    .fontColor(.primary)
+                    .alignFullWidth()
+
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]) {
+                        ForEach(BodyPart.allCases, id: \.self) { bodyPart in
+                            Button {
+                                if generationInfo.hurtingParts.contains(bodyPart) {
+                                    generationInfo.hurtingParts.removeAll {
+                                        $0 == bodyPart
+                                    }
+                                } else {
+                                    generationInfo.hurtingParts.append(bodyPart)
                                 }
-                            } else {
-                                generationInfo.hurtingParts.append(bodyPart)
+                            } label: {
+                                let selected = generationInfo.hurtingParts.contains(bodyPart)
+                                RoundedRectangle(cornerRadius: 6)
+                                    .fill(selected ? Color.orange : Color.clear)
+                                    .frame(height: 50)
+                                    .overlay {
+                                        Text(bodyPart.rawValue)
+                                            .foregroundColor(selected ? Color.white : Color("TextSecondary"))
+                                            .fontWeight(selected ? .bold : .regular)
+                                            .padding(.vertical, 6)
+                                            .padding(.horizontal, 12)
+                                    }
                             }
-                        } label: {
-                            Text(bodyPart.rawValue)
-                                .fontColor(generationInfo.hurtingParts.contains(bodyPart) ? .primary : .secondary)
-                                .padding(.vertical, 6)
-                                .padding(.horizontal, 12)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 6)
-                                        .fill(generationInfo.hurtingParts
-                                            .contains(bodyPart) ? Color("TextMuted") : Color("Background"))
-                                )
                         }
                     }
                 }
+
+                Text("Any other notes?")
+                    .font(.body)
+                    .fontWeight(.bold)
+                    .fontColor(.primary)
+                    .alignFullWidth()
+
+                TextField("Notes", text: $generationInfo.notes)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.bottom, 20)
             }
 
             Button {
