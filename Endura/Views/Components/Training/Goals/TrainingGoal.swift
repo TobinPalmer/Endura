ef526bb572a6c74aec55ea0ba8dc6cb3a3d7fdd6
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-28 18:30:45 -0700

    Fixed minor styling for training goals

--- Endura/Views/Components/Training/Goals/TrainingGoal.swift
+++ Endura/Views/Components/Training/Goals/TrainingGoal.swift
@@ -35,8 +35,6 @@ public struct TrainingGoal: View {
 
             ActivityPostStats(distance: goal.getDistance(), duration: goal.getTime())
 
-            Divider()
-
             VStack {
                 if let preRoutine = goal.preRoutine {
                     NavigationLink(destination: RoutineStartView(preRoutine)) {
