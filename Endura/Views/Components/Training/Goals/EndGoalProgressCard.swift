e0cbee2167323b4d395a9784875fe3c17f9e3ae0
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-31 07:52:17 -0700

    Removed the link in the edit view

--- Endura/Views/Components/Training/Goals/EndGoalProgressCard.swift
+++ Endura/Views/Components/Training/Goals/EndGoalProgressCard.swift
@@ -10,80 +10,85 @@ struct EndGoalProgressCard: View {
     var body: some View {
         VStack {
             if let endTrainingGoal = endGoal ?? activeUser.training.endTrainingGoal {
-                NavigationLink(destination: TrainingEndGoalSetupView(endTrainingGoal)) {
-                    HStack(spacing: 10) {
-                        let progressRingSize = 100
-                        if endTrainingGoal.completed {
-                            ZStack {
-                                Circle()
-                                    .stroke(Color.accentColor.opacity(0.2), lineWidth: 8)
-                                    .frame(width: CGFloat(progressRingSize), height: CGFloat(progressRingSize))
-                                Circle()
-                                    .trim(from: 0, to: 1)
-                                    .stroke(Color.accentColor, style: StrokeStyle(lineWidth: 8, lineCap: .round))
-                                    .frame(width: CGFloat(progressRingSize), height: CGFloat(progressRingSize))
-                                    .rotationEffect(.degrees(-90))
-                                VStack {
-                                    Text("Done!")
-                                        .font(.title3)
-                                        .fontWeight(.bold)
-                                        .fontColor(.primary)
-                                    Text("🎉")
-                                        .font(.title)
-                                        .fontColor(.secondary)
-                                }
-                            }
-                        } else {
-                            ZStack {
-                                Circle()
-                                    .stroke(Color.accentColor.opacity(0.2), lineWidth: 8)
-                                    .frame(width: CGFloat(progressRingSize), height: CGFloat(progressRingSize))
-                                Circle()
-                                    .trim(from: 0, to: CGFloat(endTrainingGoal.getProgress()))
-                                    .stroke(Color.accentColor, style: StrokeStyle(lineWidth: 8, lineCap: .round))
-                                    .frame(width: CGFloat(progressRingSize), height: CGFloat(progressRingSize))
-                                    .rotationEffect(.degrees(-90))
-                                VStack {
-                                    Text("\(endTrainingGoal.daysLeft())")
-                                        .font(.title2)
-                                        .fontWeight(.bold)
-                                        .fontColor(.primary)
-                                    Text("days left")
-                                        .font(.caption)
-                                        .fontColor(.secondary)
-                                }
+                let content = HStack(spacing: 10) {
+                    let progressRingSize = 100
+                    if endTrainingGoal.completed {
+                        ZStack {
+                            Circle()
+                                .stroke(Color.accentColor.opacity(0.2), lineWidth: 8)
+                                .frame(width: CGFloat(progressRingSize), height: CGFloat(progressRingSize))
+                            Circle()
+                                .trim(from: 0, to: 1)
+                                .stroke(Color.accentColor, style: StrokeStyle(lineWidth: 8, lineCap: .round))
+                                .frame(width: CGFloat(progressRingSize), height: CGFloat(progressRingSize))
+                                .rotationEffect(.degrees(-90))
+                            VStack {
+                                Text("Done!")
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                    .fontColor(.primary)
+                                Text("🎉")
+                                    .font(.title)
+                                    .fontColor(.secondary)
                             }
                         }
-                        VStack(alignment: .leading, spacing: 6) {
-                            Text("\(FormattingUtils.formatMiles(endTrainingGoal.distance)) miles")
-                                .font(.title2)
-                                .fontWeight(.bold)
-                                .fontColor(.primary)
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text("Current")
-                                        .font(.caption)
-                                        .fontColor(.muted)
-                                    Text("\(FormattingUtils.secondsToFormattedTime(endTrainingGoal.currentTime))")
-                                        .fontWeight(.bold)
-                                        .fontColor(.secondary)
-                                }
-                                VStack(alignment: .leading) {
-                                    Text("Goal")
-                                        .font(.caption)
-                                        .fontColor(.muted)
-                                    Text("\(FormattingUtils.secondsToFormattedTime(endTrainingGoal.time))")
-                                        .fontWeight(.bold)
-                                        .fontColor(.secondary)
-                                }
-                                .padding(.leading, 6)
+                    } else {
+                        ZStack {
+                            Circle()
+                                .stroke(Color.accentColor.opacity(0.2), lineWidth: 8)
+                                .frame(width: CGFloat(progressRingSize), height: CGFloat(progressRingSize))
+                            Circle()
+                                .trim(from: 0, to: CGFloat(endTrainingGoal.getProgress()))
+                                .stroke(Color.accentColor, style: StrokeStyle(lineWidth: 8, lineCap: .round))
+                                .frame(width: CGFloat(progressRingSize), height: CGFloat(progressRingSize))
+                                .rotationEffect(.degrees(-90))
+                            VStack {
+                                Text("\(endTrainingGoal.daysLeft())")
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                    .fontColor(.primary)
+                                Text("days left")
+                                    .font(.caption)
+                                    .fontColor(.secondary)
+                            }
+                        }
+                    }
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text("\(FormattingUtils.formatMiles(endTrainingGoal.distance)) miles")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .fontColor(.primary)
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("Current")
+                                    .font(.caption)
+                                    .fontColor(.muted)
+                                Text("\(FormattingUtils.secondsToFormattedTime(endTrainingGoal.currentTime))")
+                                    .fontWeight(.bold)
+                                    .fontColor(.secondary)
+                            }
+                            VStack(alignment: .leading) {
+                                Text("Goal")
+                                    .font(.caption)
+                                    .fontColor(.muted)
+                                Text("\(FormattingUtils.secondsToFormattedTime(endTrainingGoal.time))")
+                                    .fontWeight(.bold)
+                                    .fontColor(.secondary)
                             }
                             .padding(.leading, 6)
-                            Spacer()
                         }
-                        .padding()
+                        .padding(.leading, 6)
+                        Spacer()
                     }
-                    .frame(height: 150)
+                    .padding()
+                }
+                .frame(height: 150)
+                if endGoal == nil {
+                    NavigationLink(destination: TrainingEndGoalSetupView(endTrainingGoal)) {
+                        content
+                    }
+                } else {
+                    content
                 }
             } else {
                 VStack(spacing: 10) {
