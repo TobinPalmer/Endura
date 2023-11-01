b1aa21c48470c16a95f5dddddfdc596946fe1d9f
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-31 18:20:23 -0700

    Styled custom generate training goals

--- Endura/Views/Training/GenerateTrainingGoalsView.swift
+++ Endura/Views/Training/GenerateTrainingGoalsView.swift
@@ -4,6 +4,7 @@ import SwiftUICalendar
 
 public struct TrainingGenerationCustomOptions {
     public var easyPace: Double = 510
+    public var hardPace: Double = 420
     public var endDate: YearMonthDay
     public var notes: String
 }
@@ -26,26 +27,30 @@ struct GenerateTrainingGoalsView: View {
     @State private var generationTask: Task<Void, Never>? = nil
 
     var body: some View {
-        VStack(spacing: 10) {
-            Text("This will generate training goals for you based on your end goal or a custom request.")
-                .font(.body)
-                .fontColor(.primary)
-                .alignFullWidth()
+        ZStack {
+            Color("Background")
+                .ignoresSafeArea()
 
-            Picker("Generate for goal", selection: $generationType) {
-                ForEach(TrainingGenerationType.allCases, id: \.self) { type in
-                    Text(type.rawValue)
+            VStack(spacing: 10) {
+                Text("This will generate training goals for you based on your end goal or a custom request.")
+                    .font(.body)
+                    .fontColor(.primary)
+                    .alignFullWidth()
+
+                Picker("Generate for goal", selection: $generationType) {
+                    ForEach(TrainingGenerationType.allCases, id: \.self) { type in
+                        Text(type.rawValue)
+                    }
                 }
-            }
-            .pickerStyle(.segmented)
-            .padding(.vertical, 8)
+                .pickerStyle(.segmented)
+                .padding(.vertical, 8)
 
-            Divider()
-                .padding(.bottom, 10)
+                Divider()
+                    .padding(.bottom, 10)
 
-            switch generationType {
-            case .goal:
-                if let endGoal = activeUser.training.endTrainingGoal {
+                switch generationType {
+                case .goal:
+                    if let endGoal = activeUser.training.endTrainingGoal {
 //                    Text(
 //                        "This will generate the next \(endGoal.daysLeft()) days to help you reach your end goal by
 //                        \(FormattingUtils.dateToFormattedDay(endGoal.date)). Your current end goal is getting
@@ -56,127 +61,165 @@ struct GenerateTrainingGoalsView: View {
 //                        .fontColor(.secondary)
 //                        .alignFullWidth()
 
-                    Text("This will generate a training plan to help you reach your end goal.")
-                        .font(.body)
-                        .fontColor(.primary)
-                        .alignFullWidth()
-                    EndGoalProgressCard()
+                        Text("This will generate a training plan to help you reach your end goal.")
+                            .font(.body)
+                            .fontColor(.primary)
+                            .alignFullWidth()
+                        EndGoalProgressCard()
 //                    Text("You can change your end goal in the profile tab.")
 //                        .font(.caption)
 //                        .fontColor(.secondary)
 //                        .alignFullWidth()
 
-                    Text("Enter any notes or further requests you have for your training.")
+                        Text("Enter any notes or further requests you have for your training.")
+                            .font(.body)
+                            .fontColor(.primary)
+                            .alignFullWidth()
+                            .padding(.top, 10)
+
+                        TextField("Notes or training requests...", text: $endGoalNotes, axis: .vertical)
+                            .lineLimit(4 ... 6)
+                            .textFieldStyle(.roundedBorder)
+                    } else {
+                        Text("No training goal")
+                        NavigationLink(destination: TrainingEndGoalSetupView()) {
+                            Text("Setup Training Goal")
+                        }
+                    }
+                case .custom:
+                    Text("How far do you want to generate this training plan for?")
+                        .font(.body)
+                        .fontColor(.primary)
+                        .alignFullWidth()
+
+                    DatePicker(
+                        selection: Binding<Date>(
+                            get: { customOptions.endDate.getDate() },
+                            set: { customOptions.endDate = $0.toYearMonthDay() }
+                        ),
+                        in: Date()...,
+                        displayedComponents: [.date]
+                    ) {
+                        Label("Completion Date", systemImage: "calendar")
+                            .font(.body)
+                            .fontColor(.primary)
+                    }
+
+                    Text("Please input your paces for accurate training.")
                         .font(.body)
                         .fontColor(.primary)
                         .alignFullWidth()
                         .padding(.top, 10)
+                        .padding(.bottom, 4)
 
-                    TextField("Notes or training requests...", text: $endGoalNotes, axis: .vertical)
-                        .lineLimit(4 ... 6)
-                        .textFieldStyle(.roundedBorder)
-                } else {
-                    Text("No training goal")
-                    NavigationLink(destination: TrainingEndGoalSetupView()) {
-                        Text("Setup Training Goal")
+                    VStack(spacing: 4) {
+                        HStack {
+                            Label("Easy Pace", systemImage: "timer")
+                                .font(.body)
+                                .fontColor(.primary)
+
+                            Spacer()
+
+                            TimeInput(time: $customOptions.easyPace, hours: false)
+                        }
+                        HStack {
+                            Label("Hard Pace", systemImage: "timer")
+                                .font(.body)
+                                .fontColor(.primary)
+
+                            Spacer()
+
+                            TimeInput(time: $customOptions.hardPace, hours: false)
+                        }
                     }
-                }
-            case .custom:
-                Text("What is the date you want to end your training?")
+                    .padding(.bottom, 10)
+
+                    Text(
+                        "Enter what you want your training to be like. For example, \"I want to run 3-4 miles every day and improve core strength\""
+                    )
                     .font(.body)
                     .fontColor(.primary)
                     .alignFullWidth()
-
-                DatePicker(
-                    selection: Binding<Date>(
-                        get: { customOptions.endDate.getDate() },
-                        set: { customOptions.endDate = $0.toYearMonthDay() }
-                    ),
-                    in: Date()...,
-                    displayedComponents: [.date]
-                ) {
-                    Label("Completion Date", systemImage: "calendar")
-                }
-
-                Text("What is pace (per mile) do you want to run at for easy runs?")
-                    .font(.body)
-                    .fontColor(.primary)
-                    .alignFullWidth()
-
-                TimeInput(time: $customOptions.easyPace, hours: false)
-
-                Text(
-                    "Enter what you want your training to be like. For example, \"I want to run 3-4 miles every day and improve core strength\""
-                )
-                .font(.body)
-                .fontColor(.secondary)
-                .alignFullWidth()
-                TextField("Custom", text: $customOptions.notes)
+                    TextField(
+                        "Enter your goal and requests for training here...",
+                        text: $customOptions.notes,
+                        axis: .vertical
+                    )
+                    .lineLimit(4 ... 6)
                     .textFieldStyle(.roundedBorder)
-            }
-
-            Text(output)
-
-            Spacer()
-
-            if progress > 0 {
-                ProgressView(value: Double(progress), total: 100)
-                    .animation(.easeInOut(duration: 5))
-                    .padding(.top, 10)
-            }
-
-            if generationTask != nil {
-                Button {
-                    generationTask?.cancel()
-                    generationTask = nil
-                    progress = 0
-                } label: {
-                    HStack {
-                        ProgressView()
-                        Text("Generating, Click to Cancel")
-                            .padding(.leading, 10)
-                    }
                 }
-                .buttonStyle(EnduraNewButtonStyle())
-            } else {
-                Button {
-                    generationTask = Task {
+
+                Text(output)
+
+                if progress > 0 {
+                    ProgressView(value: Double(progress), total: 100)
+                        .animation(.easeInOut(duration: 5))
+                        .padding(.top, 10)
+                }
+
+                if generationTask != nil {
+                    Button {
+                        generationTask?.cancel()
+                        generationTask = nil
                         progress = 0
-                        let trainingData = await TrainingGenerationUtils.generateTrainingGoals(
-                            activeUser: activeUser,
-                            generationType: generationType,
-                            infoText: generationType == .goal ? endGoalNotes : customOptions.notes,
-                            customOptions: customOptions,
-                            progress: { progress in
-                                withAnimation {
-                                    self.progress = progress
+                    } label: {
+                        HStack {
+                            ProgressView()
+                            Text("Generating, Click to Cancel")
+                                .padding(.leading, 10)
+                        }
+                    }
+                    .buttonStyle(EnduraNewButtonStyle())
+                } else {
+                    Button {
+                        generationTask = Task {
+                            progress = 0
+                            let trainingData = await TrainingGenerationUtils.generateTrainingGoals(
+                                activeUser: activeUser,
+                                generationType: generationType,
+                                infoText: generationType == .goal ? endGoalNotes : customOptions.notes,
+                                customOptions: customOptions,
+                                progress: { progress in
+                                    withAnimation {
+                                        self.progress = progress
+                                    }
                                 }
-                            }
-                        )
-                        if let trainingData = trainingData {
-                            DispatchQueue.main.async {
-                                for month in trainingData {
-                                    print("Generated \(month.key) with \(month.value.days.count) days")
-                                    activeUser.training.monthlyTrainingData.updateValue(month.value, forKey: month.key)
-                                    activeUser.training.saveTrainingMonth(month.key)
-                                    if generationTask != nil {
-                                        generationTask = nil
-                                        dismiss()
+                            )
+                            if let trainingData = trainingData {
+                                DispatchQueue.main.async {
+                                    for month in trainingData {
+                                        print("Generated \(month.key) with \(month.value.days.count) days")
+                                        activeUser.training.monthlyTrainingData.updateValue(
+                                            month.value,
+                                            forKey: month.key
+                                        )
+                                        activeUser.training.saveTrainingMonth(month.key)
+                                        if generationTask != nil {
+                                            generationTask = nil
+                                            dismiss()
+                                        }
                                     }
                                 }
                             }
                         }
+                    } label: {
+                        HStack {
+                            Image(systemName: "sparkles")
+                            Text("Generate")
+                        }
                     }
-                } label: {
-                    HStack {
-                        Image(systemName: "sparkles")
-                        Text("Generate")
+                    .buttonStyle(EnduraNewButtonStyle())
+                }
+            }
+            .enduraPadding()
+            .navigationBarTitle("Generate Training Goals", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {} label: {
+                        Image(systemName: "info.circle")
                     }
                 }
-                .buttonStyle(EnduraNewButtonStyle())
             }
         }
-        .enduraPadding()
-        .navigationBarTitle("Generate Training Goals", displayMode: .inline)
     }
 }
