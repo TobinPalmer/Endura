b28255c13fe48a8b3014077369f7be4c7d5f8afd
TobinPalmer <tobinkpalmer@gmail.com>
2023-11-27 14:00:47 -0800

    Refactored achivements list

--- Endura/Views/Components/Training/Achievements/AchievementsList.swift
+++ Endura/Views/Components/Training/Achievements/AchievementsList.swift
@@ -6,8 +6,8 @@ struct AchievementsList: View {
         ScrollView(.vertical) {
             LazyVGrid(columns: Array(repeating: .init(.flexible(), spacing: 16), count: 2), spacing: 16) {
                 AchievementCard(time: 300, title: "1 Mile")
-                AchievementCard(time: 1080, title: "5k Run")
-                AchievementCard(time: 2240, title: "10k Run")
+                AchievementCard(time: 1110, title: "5k Run")
+                AchievementCard(time: 2483, title: "10k Run")
                 AddAchievementCard()
             }
         }
